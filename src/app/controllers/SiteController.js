e83dfcb5bea5b492c1fc719e1dbf922d77177e95
kyle <leanhtuanlhln@gmail.com>
2022-05-28 23:13:16 +0700

    MVC pattern and create home page

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,6 +1,15 @@
+const Course = require("../models/Course");
+const { multipleMongooseToObject } = require("../../ultil/mongoose");
+
 class SiteController {
-  index(req, res) {
-    res.render("home");
+  index(req, res, next) {
+    Course.find({})
+      .then((courses) => {
+        res.render("home", {
+          courses: multipleMongooseToObject(courses),
+        });
+      })
+      .catch((err) => next(error));
   }
   search(req, res) {
     res.render("search");
--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-const express = require('express')
-const morgan = require('morgan')
-const { engine } = require('express-handlebars');
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require("./routes");
+const db = require("./config/db");
+
+//Connect to db
+db.connect();
 
 //middleware
 // app.use(morgan('combined'))
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs')
-app.set('views',path.join(__dirname, 'resource/views'))
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resource", "views"));
 
-route(app)
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
